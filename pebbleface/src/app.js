31baa91e65b1619e76c7f999b3e83d557f701a9f
Dan Russell <danrussell1@hotmail.com>
2016-03-14 21:21:30 -0500

    Updated error UI card

--- pebbleface/src/app.js
+++ pebbleface/src/app.js
@@ -1,28 +1,34 @@
-/**
- * Welcome to Pebble.js!
- *
- * This is where you write your app.
- */
-
 var ajax = require('ajax');
 var UI = require('ui');
 
-ajax({url: 'http://98.226.222.143:8181/climate', type: 'json'},
-function(data) {
-  var content = 'Temperature: ' + data.temperature + ' °F\n' +
-      'Humidity:\n' + data.humidity + '%\n' +
-      'Heat Index:\n' + data.heatIndex + ' °F\n' +
-      'Last Recorded:\n' + data.timestamp;
-  
-  var boomCard = new UI.Card({
-    title: 'Office Data',
-    body: content,  
-    scrollable: true
-  });
-  boomCard.show();
-   
-},
-function(error) {
-console.log('Ajax failed: ' + error);
-}
+ajax(
+  {
+    url: 'http://98.226.222.143:8181/climate', 
+    type: 'json'
+  },
+  function(data) {
+    var content = 'Temperature: ' + data.temperature + ' °F\n' +
+        'Humidity:\n' + data.humidity + '%\n' +
+        'Heat Index:\n' + data.heatIndex + ' °F\n' +
+        'Last Recorded:\n' + data.timestamp;
+    
+    var officeCard = new UI.Card({
+      title: 'Office Data',
+      body: content,  
+      scrollable: true,
+      backgroundColor: 'green'
+    });
+    
+    officeCard.show();
+  },
+  function(error) {
+    var officeCard = new UI.Card({
+      title: 'Office Data',
+      body: 'An error occurred',  
+      scrollable: true,
+      backgroundColor: 'red'
+    });
+    
+    officeCard.show();
+  }
 );
